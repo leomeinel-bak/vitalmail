9a0a3410dd797789a71f6d0f6024e0833af98a8a
TamrielNetwork <contact@tamriel.me>
2022-03-14 18:59:41 +0100

    Reformat code

--- src/main/java/com/tamrielnetwork/vitalhome/storage/MailStorageSql.java
+++ src/main/java/com/tamrielnetwork/vitalhome/storage/MailStorageSql.java
@@ -48,7 +48,8 @@ public class MailStorageSql
 	public List<Map<String, String>> loadMail(@NotNull String receiverUUID) {
 		List<Map<String, String>> mailList = new ArrayList<>();
 		try (PreparedStatement selectStatement = SqlManager.getConnection()
-		                                                   .prepareStatement("SELECT * FROM " + Sql.getPrefix() + "Mail")) {
+		                                                   .prepareStatement(
+				                                                   "SELECT * FROM " + Sql.getPrefix() + "Mail")) {
 			try (ResultSet rs = selectStatement.executeQuery()) {
 				while (rs.next()) {
 					if (!Objects.equals(rs.getString(1), receiverUUID) || rs.getString(1) == null) {
@@ -102,8 +103,8 @@ public class MailStorageSql
 	@Override
 	public void clear(@NotNull String receiverUUID) {
 		try (PreparedStatement deleteStatement = SqlManager.getConnection()
-		                                                   .prepareStatement(
-				                                                   "DELETE FROM " + Sql.getPrefix() + "Mail WHERE `ReceiverUUID`=?")) {
+		                                                   .prepareStatement("DELETE FROM " + Sql.getPrefix()
+		                                                                     + "Mail WHERE `ReceiverUUID`=?")) {
 			deleteStatement.setString(1, receiverUUID);
 			deleteStatement.executeUpdate();
 		}
